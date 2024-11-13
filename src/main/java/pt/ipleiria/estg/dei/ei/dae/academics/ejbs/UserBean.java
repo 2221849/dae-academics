16880b810c6068c78721cc9fd04e122303b2875c
2221849 <2221849@my.ipleiria.pt>
2024-11-13 12:10:50 +0000

    Worksheet 7 - Part II

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/UserBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/UserBean.java
@@ -1,9 +1,12 @@
 package pt.ipleiria.estg.dei.ei.dae.academics.ejbs;
 
 import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.hibernate.Hibernate;
 import pt.ipleiria.estg.dei.ei.dae.academics.entities.User;
+import pt.ipleiria.estg.dei.ei.dae.academics.security.Hasher;
 
 @Stateless
 public class UserBean {
@@ -11,8 +14,22 @@ public class UserBean {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Inject
+    private Hasher hasher;
+
     public void create(String username, String password, String name, String email) {
         var user = new User(username, password, name, email);
         entityManager.persist(user);
+    }
+
+    public User findOrFail(String username) {
+        var user = entityManager.getReference(User.class, username);
+        Hibernate.initialize(user);
+        return user;
+    }
+
+    public boolean canLogin(String username, String password) {
+        var user = findOrFail(username);
+        return user != null && user.getPassword().equals(hasher.hash(password));
     }
 }
